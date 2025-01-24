5390dae43a68953f3b48593d8ed0e0f9a53f3a5f
Diego Caceres <dado90caceres@gmail.com>
2025-01-24 12:56:07 -0300

    Test vercel deploy

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ export default function Home() {
     <div className="text-center">
       <h1 className="text-4xl font-bold mt-10">Explora Nuestros Viajes de Surf</h1>
       <p className="mt-4 text-lg text-gray-700">Descubre los mejores destinos para surfear este año.</p>
+      <p className="mt-4 text-lg text-gray-700">Disfruta de este deporte acompañado.</p>
     </div>
   );
 }
