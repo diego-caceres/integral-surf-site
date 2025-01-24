5d4a0184eded9fc6c0dd6d5a24c76f2d5b45c00b
Diego Caceres <dado90caceres@gmail.com>
2025-01-24 13:32:01 -0300

    Use GoogleTagManager component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-import Script from "next/script";
+import { GoogleTagManager } from "@next/third-parties/google";
 import "../styles/globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
@@ -6,30 +6,12 @@ import Footer from "@/components/Footer";
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="es">
-      <head>
-       <Script id="google-analytics" strategy="afterInteractive">
-        {`
-          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-          })(window,document,'script','dataLayer','GTM-P874N777');
-        `}
-       </Script>
-      </head>
-      <body className="bg-gray-100 text-gray-900">
-        <noscript>
-          <iframe 
-            src="https://www.googletagmanager.com/ns.html?id=GTM-P874N777"
-            height="0" 
-            width="0" 
-            style={{ display:"none", visibility:"hidden"}}
-          ></iframe>
-        </noscript>
+      <body className="bg-gray-100 text-gray-900">        
         <Navbar />
         <main className="container mx-auto p-4">{children}</main>
         <Footer />
       </body>
+      <GoogleTagManager gtmId="GTM-P874N777" /> 
     </html>
   );
 }
