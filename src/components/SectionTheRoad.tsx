23a99b7ef5027da4b2be48795a35f4ea07e7dd55
Diego Caceres <dado90caceres@gmail.com>
2025-02-11 17:03:05 -0300

    Fix the road

--- src/components/SectionTheRoad.tsx
+++ src/components/SectionTheRoad.tsx
@@ -15,24 +15,9 @@ export default function SectionTheRoad() {
 
       {/* Content Overlay */}
       <div className="absolute inset-0 flex items-center justify-center p-6 ">
-        <div className="grid grid-cols-1 md:grid-cols-2 max-w-5xl w-full gap-6 text-white">
-          {/* Right Column (Shown first on mobile) */}
-          <div className="md:max-w-[450px] flex flex-col items-center justify-center  gap-4 order-1 md:order-none">
-            <h2 className="uppercase font-[Eckmannpsych] md:text-left text-white ">
-              El camino del surf no es solitario
-            </h2>
-            <p className="text-white md:text-left text-lg">
-              juntos aprendemos más y mejor, <br />
-              compartir es un principio básico del surf <br />
-              desde sus orígenes, y es una <br />
-              oportunidad acompañarnos frente a los <br />
-              desafíos que el mar nos propone.
-            </p>
-            <Button className="mt-4 text-lg">Descubre más</Button>
-          </div>
-
+        <div className="flex flex-col md:grid md:grid-cols-2 max-w-5xl w-full gap-6 text-white">
           {/* Left Column (Shown below on mobile) */}
-          <div className="flex flex-col md:gap-8 justify-center order-2 md:order-none">
+          <div className="flex flex-col md:gap-8 justify-center order-2 md:order-1 ">
             <Image
               src="/images/home/the-road-img1.png"
               alt="Left Image 1"
@@ -45,6 +30,21 @@ export default function SectionTheRoad() {
               width={391}
               height={230}
             />
+          </div>
+
+          {/* Right Column (Shown first on mobile) */}
+          <div className="md:max-w-[450px] flex flex-col items-center md:items-start justify-center  gap-4 order-1 md:order-2 ">
+            <h2 className="uppercase font-[Eckmannpsych] md:text-left text-white ">
+              El camino del surf no es solitario
+            </h2>
+            <p className="text-white md:text-left text-lg">
+              juntos aprendemos más y mejor, <br />
+              compartir es un principio básico del surf <br />
+              desde sus orígenes, y es una <br />
+              oportunidad acompañarnos frente a los <br />
+              desafíos que el mar nos propone.
+            </p>
+            <Button className="mt-4 text-lg">Descubre más</Button>
           </div>
         </div>
       </div>
