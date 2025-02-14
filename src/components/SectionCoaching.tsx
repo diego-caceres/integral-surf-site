f4bd62abcd665568cfc9b70a468f147ab232ad55
Diego Caceres <dado90caceres@gmail.com>
2025-02-14 11:16:20 -0300

    Improve mobile styles

--- src/components/SectionCoaching.tsx
+++ src/components/SectionCoaching.tsx
@@ -3,34 +3,35 @@ import Button from "@/components/ui/Button";
 
 export default function SectionCoaching() {
   return (
-    <div className="relative w-full h-screen overflow-hidden ">
+    <div className="relative w-full h-screen overflow-hidden pt-20">
       {/* Content Overlay */}
-      <div className="absolute inset-0 flex items-center justify-center p-6 ">
+      <div className="absolute inset-0 flex items-center justify-center p-6 pb-10">
         <div className="flex flex-col md:grid md:grid-cols-2 md:pl-[100px] w-full gap-6 ">
           {/* Left Column (Shown below on mobile) */}
-          <div className="flex flex-col md:gap-8 justify-center order-2 md:order-1 ">
+          <div className="flex flex-col gap-4 md:gap-8 justify-center order-2 md:order-1 ">
             <Image
               src="/images/home/coaching.jpg"
               alt="Left Image 1"
               width={488}
               height={594}
               style={{ objectFit: "cover" }}
+              className="max-w-full"
             />
           </div>
 
           {/* Right Column (Shown first on mobile) */}
           <div className="mt-10 md:mt-0 md:max-w-[450px] flex flex-col items-center md:items-start justify-center gap-4 order-1 md:order-2 ">
-            <h2 className="uppercase font-[Eckmannpsych] md:text-left  ">
+            <h2 className="uppercase font-[Eckmannpsych] md:text-left text-3xl md:text-5xl">
               La importancia del coaching
             </h2>
-            <p className=" md:text-left text-xl">
+            <p className=" md:text-left text-lg md:text-xl">
               surf coachs profesionales y experientes <br />
               nos acompañan compartiéndonos este <br />
               deporte desde sus raíces. buscamos <br />
               aprender la técnica y teoría del surf en <br />
               profundidad.
             </p>
-            <Button className="mt-4 text-xl">Descubre más</Button>
+            <Button className="mt-4 text-lg md:text-xl">Descubre más</Button>
           </div>
         </div>
       </div>
--- src/components/SectionTheRoad.tsx
+++ src/components/SectionTheRoad.tsx
@@ -3,7 +3,7 @@ import Button from "@/components/ui/Button";
 
 export default function SectionTheRoad() {
   return (
-    <div className="relative w-full h-screen overflow-hidden ">
+    <div className="relative w-full h-screen overflow-hidden pt-20">
       {/* Background Image */}
       <Image
         src="/images/home/sea-background.jpeg"
@@ -14,37 +14,39 @@ export default function SectionTheRoad() {
       />
 
       {/* Content Overlay */}
-      <div className="absolute inset-0 flex items-center justify-center p-6 ">
+      <div className="absolute inset-0 flex items-center justify-center p-6 pb-10">
         <div className="flex flex-col md:grid md:grid-cols-2 md:pl-[100px] w-full gap-6 text-white">
           {/* Left Column (Shown below on mobile) */}
-          <div className="flex flex-col md:gap-8 justify-center order-2 md:order-1 ">
+          <div className="flex flex-col gap-4 md:gap-8 justify-center order-2 md:order-1 ">
             <Image
               src="/images/home/the-road-img1.png"
               alt="Left Image 1"
               width={450}
               height={380}
+              className="max-w-full"
             />
             <Image
               src="/images/home/the-road-img2.png"
               alt="Left Image 2"
               width={450}
               height={274}
+              className="max-w-full"
             />
           </div>
 
           {/* Right Column (Shown first on mobile) */}
           <div className="mt-10 md:mt-0 md:max-w-[450px] flex flex-col items-center md:items-start justify-center  gap-4 order-1 md:order-2 ">
-            <h2 className="uppercase font-[Eckmannpsych] md:text-left text-white ">
+            <h2 className="uppercase font-[Eckmannpsych] md:text-left text-white text-3xl md:text-5xl">
               El camino del surf no es solitario
             </h2>
-            <p className="text-white md:text-left text-xl">
+            <p className="text-white md:text-left text-lg md:text-xl">
               juntos aprendemos más y mejor, <br />
               compartir es un principio básico del surf <br />
               desde sus orígenes, y es una <br />
               oportunidad acompañarnos frente a los <br />
               desafíos que el mar nos propone.
             </p>
-            <Button className="mt-4 text-xl">Descubre más</Button>
+            <Button className="mt-4 text-lg md:text-xl">Descubre más</Button>
           </div>
         </div>
       </div>
